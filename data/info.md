b4883af63743f3ab5c7fb2dd73b57b8e865aa246
swae-way <lee0swee@gmail.com>
2025-04-05 18:44:31 -0400

    Initial commit

---
+++ data/info.md
@@ -0,0 +1,24 @@
+# CNQueer-story
+
+The [CNQueer-story](https://lee0swee.wixsite.com/cnqueerstory) 
+
+![tags](img/infobar_tags.svg)
+
+CNQueerstory致力于呈现中国大陆LGBTQ历史事件的可视化展示，通过时间轴清晰展示每个重要事件。网站上方的关键词代表重要议题或具体年份。
+CNQueerstory is dedicated to presenting a visual display of LGBTQ historical events in mainland China, clearly showcasing each important event through a timeline. The keywords above the website represent prominent topics. 
+
+网站底部的时间轴分为四个阶段，这些阶段是根据中国官方对LGBTQ+群体的政策态度变化而划分的。分别为：转向开放（1997-2001）、日益包容与认可（2001-2015）、管控收紧与审查加强（2015-2020）、模糊的边界与暧昧的政策（2020-至今）。
+The timeline at the bottom of the website is divided into four periods, categorized according to the Chinese government's policy attitudes toward LGBTQ+ communities: Opening Up (1997-2001), Emerging Tolerance and Semi-Recognition (2001-2015), Tightening Control and Censorship (2015-2020), and Elusive Boundaries and Ambiguous Policies (2020-Present). 
+
+网站上方设有一系列关键词筛选器，用户可以按照具体年份或事件类型来筛选和查看相关历史事件。
+On the top of the website, a series of keywords allows users to filter events by specific years or different event types.
+
+![time](img/infobar_time.svg)
+
+事件和故事按时间顺序排列。在年份下方，您可以找到更多传记性信息。
+The events and stories are positioned diachronically. Below the year you can find further biographical information.
+
+![time](img/infobar_scroll.svg)
+
+您可以使用触摸板或滚轮在艺术作品区域进行缩放，以查看更多细节。点击并拖动背景可以调整可见区域。
+You can either zoom with your touchpad or scroll wheel in the areas of the artworks in order to see more details. Klick and drag the background in order to adjust the visible area.
---
+++ js/viz.js
@@ -0,0 +1,208 @@
+//                            ,--.
+//                ,---,   ,--/  /|               .--.--.
+//        ,---.,`--.' |,---,': / '         ,--, /  /    '.
+//       /__./||   :  ::   : '/ /        ,'_ /||  :  /`. /
+//  ,---.;  ; |:   |  '|   '   ,    .--. |  | :;  |  |--`
+// /___/ \  | ||   :  |'   |  /   ,'_ /| :  . ||  :  ;_
+// \   ;  \ ' |'   '  ;|   ;  ;   |  ' | |  . . \  \    `.
+//  \   \  \: ||   |  |:   '   \  |  | ' |  | |  `----.   \
+//   ;   \  ' .'   :  ;|   |    ' :  | | :  ' ;  __ \  \  |
+//    \   \   '|   |  ''   : |.  \|  ; ' |  | ' /  /`--'  /
+//     \   `  ;'   :  ||   | '_\.':  | : ;  ; |'--'.     /
+//      :   \ |;   |.' '   : |    '  :  `--'   \ `--'---'
+//       '---" '---'   ;   |,'    :  ,      .-./
+//                     '---'       `--`----'
+//                ,---,    ,---,.           .---.    ,---,.,-.----.
+//        ,---.,`--.' |  ,'  .' |          /. ./|  ,'  .' |\    /  \
+//       /__./||   :  :,---.'   |      .--'.  ' ;,---.'   |;   :    \
+//  ,---.;  ; |:   |  '|   |   .'     /__./ \ : ||   |   .'|   | .\ :
+// /___/ \  | ||   :  |:   :  |-, .--'.  '   \' .:   :  |-,.   : |: |
+// \   ;  \ ' |'   '  ;:   |  ;/|/___/ \ |    ' ':   |  ;/||   |  \ :
+//  \   \  \: ||   |  ||   :   .';   \  \;      :|   :   .'|   : .  /
+//   ;   \  ' .'   :  ;|   |  |-, \   ;  `      ||   |  |-,;   | |  \
+//    \   \   '|   |  ''   :  ;/|  .   \    .\  ;'   :  ;/||   | ;\  \
+//     \   `  ;'   :  ||   |    \   \   \   ' \ ||   |    \:   ' | \.'
+//      :   \ |;   |.' |   :   .'    :   '  |--" |   :   .':   : :-'
+//       '---" '---'   |   | ,'       \   \ ;    |   | ,'  |   |.'
+//                     `----'          '---"     `----'    `---'
+
+// christopher pietsch
+// @chrispiecom
+// 2015-2018
+
+utils.welcome();
+
+var data;
+var tags;
+var canvas;
+var search;
+var ping;
+var timeline;
+
+if (Modernizr.webgl && !utils.isMobile()) {
+  init();
+}
+
+
+function init() {
+  canvas = Canvas();
+  search = Search();
+  timeline = Timeline();
+  ping = utils.ping();
+
+  var baseUrl = utils.getDataBaseUrl();
+  var makeUrl = utils.makeUrl;
+
+  console.log(baseUrl);
+
+  d3.json(baseUrl.config || "data/config.json", function (config) {
+    config.baseUrl = baseUrl;
+    utils.initConfig(config);
+
+    Loader(makeUrl(baseUrl.path, config.loader.timeline)).finished(function (timeline) {
+      Loader(makeUrl(baseUrl.path, config.loader.items)).finished(function (data) {
+        console.log(data);
+
+        utils.clean(data, config.delimiter);
+        
+        if(config.filter && config.filter.type === "crossfilter") {
+          tags = Crossfilter();
+        } else {
+          tags = Tags();
+        }
+        tags.init(data, config);
+        search.init();
+        canvas.init(data, timeline, config);
+
+        if (config.loader.layouts) {
+          initLayouts(config);
+        } else {
+          canvas.setMode({
+            title: "Time",
+            type: "group",
+            groupKey: "year"
+          })
+        }
+
+        // setTimeout(function () {
+        //   var idx = 102
+        //   canvas.zoomToImage(data[idx], 100)
+        // }, 100);
+
+        LoaderSprites()
+          .progress(function (textures) {
+            // Create a lookup map for faster access
+            const dataMap = new Map(
+              data
+                .filter(d => d.sprite) // Ensure sprite exists
+                .map(d => [d.id, d])
+            );
+            
+            Object.keys(textures).forEach(id => {
+              const item = dataMap.get(id);
+              if (item) item.sprite.texture = textures[id];
+            });
+            canvas.wakeup();
+          })
+          //.finished() recalculate sizes
+          .load(makeUrl(baseUrl.path, config.loader.textures.medium.url));
+      });
+    });
+  });
+
+  d3.select(window)
+    .on("resize", function () {
+      if (canvas !== undefined && tags !== undefined) {
+        clearTimeout(window.resizedFinished);
+        window.resizedFinished = setTimeout(function () {
+          canvas.resize();
+          tags.resize();
+        }, 250);
+      }
+    })
+    .on("keydown", function (e) {
+      if (d3.event.keyCode != 27) return;
+      search.reset();
+      tags.reset();
+      canvas.split();
+    });
+
+  d3.select(".filterReset").on("click", function () {
+    canvas.resetZoom(function () {
+      tags.reset();
+      //canvas.split();
+    })
+  });
+  d3.select(".filterReset").on("dblclick", function () {
+    console.log("dblclick");
+    location.reload();
+  });
+
+  d3.select(".slidebutton").on("click", function () {
+    var s = !d3.select(".sidebar").classed("sneak");
+    d3.select(".sidebar").classed("sneak", s);
+  });
+
+  d3.select(".infobutton").on("click", function () {
+    var s = !d3.select(".infobar").classed("sneak");
+    d3.select(".infobar").classed("sneak", s);
+  });
+
+  // d3.selectAll(".navi .button").on("click", function () {
+  //   var that = this;
+  //   var mode = d3.select(this).attr("data");
+  //   canvas.setMode(mode);
+  //   timeline.setDisabled(mode != "time");
+
+  //   d3.selectAll(".navi .button").classed("active", function () {
+  //     return that === this;
+  //   });
+  // });
+
+  function initLayouts(config) {
+    d3.select(".navi").classed("hide", false);
+
+    //console.log(config.loader.layouts);
+    config.loader.layouts.forEach((d, i) => {
+      // d.title = d.title.toLowerCase();
+      // legacy fix for time scales
+      if (!d.type && !d.url) {
+        d.type = "group"
+        d.groupKey = "year"
+      }
+      if (d.type === "group" && i == 0) {
+        canvas.setMode(d);
+      } else if (d.url) {
+        d3.csv(utils.makeUrl(baseUrl.path, d.url), function (tsne) {
+          canvas.addTsneData(d.title, tsne, d.scale);
+          if (i == 0) canvas.setMode(d);
+        });
+      }
+    });
+
+    if (config.loader.layouts.length == 1) {
+      d3.select(".navi").classed("hide", true);
+    }
+
+    var s = d3.select(".navi").selectAll(".button").data(config.loader.layouts);
+    s.enter()
+      .append("div")
+      .classed("button", true)
+      .classed("space", (d) => d.space)
+      .text((d) => d.title);
+
+    s.on("click", function (d) {
+      canvas.setMode(d);
+      d3.selectAll(".navi .button").classed(
+        "active",
+        (d) => d.title == canvas.getMode().title
+      );
+    });
+    d3.selectAll(".navi .button").classed(
+      "active",
+      (d) => d.title == config.loader.layouts[0].title
+    );
+  }
+}
+
+d3.select(".browserInfo").classed("show", utils.isMobile());
